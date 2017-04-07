ab082079700ea583926f2ebcc4409122cf1f2927
Hailz <hailey083@yahoo.com>
2017-04-07 01:24:46 -0700

    creating items works

--- public/app/controllers/Create_Item_Controller.js
+++ public/app/controllers/Create_Item_Controller.js
@@ -7,23 +7,28 @@ angular
     '$stateParams', 
     'AuthFactory', 
     'UserFactory', 
-    function($scope, $location, $http, $stateParams, AuthFactory, UserFactory){
-        return
+    'ItemFactory',
+    function($scope, $location, $http, $stateParams, AuthFactory, UserFactory, ItemFactory){
+    
 
-      $scope.currentUserId = $window.localStorage['currentUserId'];
+    $scope.tempUser = AuthFactory.currentUser();
+    $scope.userId = $scope.tempUser.id;
+    console.log("User id " + $scope.userId)
+
 
         $scope.newItem = {
          name: '',
          photo: '',
          price: '',
          size: '',
-         sellerId: currentUserId,
+         sellerId: $scope.userId,
          species: '',
          condition: '',
          description: '',
         }
         
     $scope.addItem = function() {
+        console.log('click')
         console.log($scope.newItem)
         ItemFactory.postItem($scope.newItem)
         .then(function success(res) {
