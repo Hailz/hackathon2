4d05a5b634d17b277de407d3c55314b77c98c2a1
jnhustin <jnhustin@gmail.com>
2017-04-06 17:47:06 -0700

    added item model

---
+++ models/item.js
@@ -0,0 +1,44 @@
+var mongoose = require('mongoose');
+var bcrypt = require('bcrypt');
+
+var ItemSchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  photo:  {
+    type: String,
+    required: true
+  },
+  price:  {
+    type: String,
+    required: true
+  },
+  size:  {
+    type: String,
+    required: true
+  },
+  species:      String,
+  condition:    String,
+  description:  String,
+});
+
+ItemSchema.set('toJSON', {
+  transform: function(doc, ret, options) {
+    var returnJson = {
+      id:           ret._id,
+      name:         ret.name,
+      photo:        ret.photo,
+      price:        ret.price,
+      size:         ret.size,
+      species:      ret.species,
+      condition:    ret.condition,
+      description:  ret.description
+    };
+    return returnJson;
+  }
+});
+
+
+
+module.exports = mongoose.model('Item', ItemSchema);
