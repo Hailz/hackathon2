77343bb7090cf49ae44d7581e43cae1a365b8929
Hailz <hailey083@yahoo.com>
2017-04-07 13:58:55 -0700

    Cleaning up profile view

--- public/app/controllers/All_Items_Controller.js
+++ public/app/controllers/All_Items_Controller.js
@@ -43,7 +43,7 @@ angular
       }
     );
     $scope.addToCart = function(item){
-      console.log(this.id)
+      console.log(item._id)
       $scope.newCartItem = {
         itemId: item.id,
         sellerId: item.sellerId,
--- public/app/controllers/Profile_Controller.js
+++ public/app/controllers/Profile_Controller.js
@@ -6,7 +6,8 @@ angular
   '$location',
   'AuthFactory',
   'UserFactory',
-  function($scope, $state, $location, AuthFactory, UserFactory) {
+  'ItemFactory',
+  function($scope, $state, $location, AuthFactory, UserFactory, ItemFactory) {
     
     //add local storage to grab currentUser
     $scope.currentUserId;
@@ -26,6 +27,23 @@ angular
           function error (err) { console.log('error in runAtPageRender()') }
         );
       }
+            // DB CALL FOR ITEMS SELLER CURRENT SELLING
+      ItemFactory.getAllItems()
+      .then(
+      function success(res) {
+      // expecting an array of all items from all users
+        $scope.allItems = res.data;
+        console.log(res.data)
+        // filter through only the comment that belongs to user
+        $scope.saleItems = $scope.allItems.filter(function(item) {
+          return item.sellerId == $scope.currentUserId
+        })
+        // show on page
+          console.log('sellerItems: ', $scope.saleItems)
+        },
+        function error(err) {
+          console.log('error in getAllItems() in itemfactory() in runAtPageRender() ', err)
+        })
     }
 
     $scope.isLoggedIn = function() {
