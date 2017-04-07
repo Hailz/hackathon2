32489e1432e38b7e3813e7ee126f9078797bdd4a
jnhustin <jnhustin@gmail.com>
2017-04-07 09:52:01 -0700

    added display for all items on home, edited home controller

--- public/app/controllers/Home_Controller.js
+++ public/app/controllers/Home_Controller.js
@@ -7,7 +7,18 @@ angular
   'UserFactory',
   'ItemFactory',
   function($scope, $state, AuthFactory, UserFactory, ItemFactory) {
-
+    $scope.items;
+    
+    ItemFactory.getAllItems()
+    .then(
+      function success(res) {
+        $scope.items = res.data;
+      },
+      function error(err) {
+        console.log('error in getSingleitem(): ', err);
+      }
+    );
+    
     $scope.isLoggedIn = function() {
         return Auth.isLoggedIn();
     }
